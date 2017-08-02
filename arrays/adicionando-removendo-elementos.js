5d0e811777444afc6e8bda3b22a244c80de31eda
Gilson Silva <gilsonsilvati@gmail.com>
2017-08-02 20:27:55 -0300

    Adicionando e removendo elementos do array

---
+++ arrays/adicionando-removendo-elementos.js
@@ -0,0 +1,29 @@
+// Cria array
+var emails = [];
+
+console.log(typeof emails);
+console.log(emails.length);
+
+// Adiciona elemento no final
+emails.push('joao@email.com');
+console.log(emails);
+
+// Adiciona elemento no final
+emails.push('maria@email.com');
+console.log(emails);
+
+// Adiciona elemento no final
+emails.push('jose@email.com');
+console.log(emails);
+
+// Remove elemento do final
+emails.pop();
+console.log(emails);
+
+// Adiciona elemento no inicio
+emails.unshift('gilsonsilvati@gmail.com');
+console.log(emails);
+
+// Remove elemento no inicio
+emails.shift();
+console.log(emails);
---
+++ arrays/criando-arrays.js
@@ -0,0 +1,16 @@
+var emails = [];
+
+emails[0] = 'joao@gmail.com';
+emails[1] = 'maria@gmail.com';
+
+console.log('emails[0] ->', emails[0]);
+console.log('emails[1] ->', emails[5]);
+
+var nomes = ['João', 'maria'];
+console.log('Quantos nomes?', nomes.length);
+
+nomes[4] = 'José';
+console.log('Quantos nomes depois do José?', nomes.length)
+
+console.log('nomes[2]', nomes[2]);
+console.log('nomes[3]', nomes[3]);
