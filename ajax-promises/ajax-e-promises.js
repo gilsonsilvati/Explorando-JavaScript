3cd2f0066d92098f591e77d85eaaa81e191725cd
Gilson Silva <gilsonsilvati@gmail.com>
2017-09-02 18:09:51 -0300

    Manipulação do DOM

--- ajax-promises/ajax-e-promises.js
+++ ajax-promises/ajax-e-promises.js
@@ -5,14 +5,16 @@ $(function() {
 		dataType: 'jsonp'
 	});
 
-	// Promises...
 	resposta.done(function(estados) {
+		var comboEstado = $('#combo-estado');
+		// comboEstado.empty();
+		comboEstado.html('<option>Selecione o estado</option>');
 		estados.forEach(function(estado) {
-			console.log(estado);
+			var optionEstado = $('<option>').val(estado.uf).text(estado.nome);
+			comboEstado.append(optionEstado);
 		});
 	});
 
-	// Promises...
 	resposta.fail(function(estados) {
 		alert('Erro carregando estados do servidor!');
 	});
