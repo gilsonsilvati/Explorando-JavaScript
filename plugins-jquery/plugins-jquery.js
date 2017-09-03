d1a7e4ea4b45472634d2413fff2f65a4d19ae519
Gilson Silva <gilsonsilvati@gmail.com>
2017-09-03 19:28:24 -0300

    Plugins jQuery

---
+++ plugins-jquery/plugins-jquery.js
@@ -0,0 +1,31 @@
+// Carrega depois da página html (DOM)
+$(function() {
+	var valorElement = $('#valor'); // jQuery
+	var totalElement = document.getElementById('total'); // JavaScript
+	var totalMascara = $('#total-mascara');
+	var mascaraConfig = {
+		decimal: ',',
+		thousands: '.',
+		prefix: 'R$ '
+	};
+
+	valorElement.on('keyup', function(e) {
+		var total = 0;
+		var valor = valorElement.maskMoney('unmasked')[0]; // Já devolve um float
+		// var valor = parseFloat(valorElement.val() ? valorElement.val() : 0);
+
+		if (valor > 0 && valor < 10) {
+			total = valor + valor * 0.3 + 5;
+		} else if (valor >= 10 && valor < 50) {
+			total = valor + valor * 0.1 + 2;
+		} else {
+			total = valor;
+		}
+
+		totalMascara.maskMoney(mascaraConfig);
+		totalMascara.maskMoney('mask', total);
+		totalElement.innerHTML = totalMascara.val();
+	});
+
+	valorElement.maskMoney(mascaraConfig);
+});
